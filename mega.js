a8857ed67cf11e837fd0ad7888a3d8f4b27a3d40
queenelsamd <mywhatsappbot79@gmail.com>
2025-02-25 00:43:29 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "tsu72220@gmail.com",
+  password: "#Radip12345678",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
